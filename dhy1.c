da674ce0868a33184de71e4d1bae93060d58be7d
little-potato-loria <tudoutudouyaaaaa@163.com>
2024-11-04 15:21:08 +0800

    done

--- dhy1.c
+++ dhy1.c
@@ -1,154 +1,122 @@
-#include <GL/glut.h>
+/*需要glut环境，右键项目，选择管理NuGet程序包，下载名为glut的两个包（下载任何一个会自动下载两个）即可*/
+#include <GL/freeglut.h>
 #include <math.h>
-#include <stdio.h>
-const double c2 = 3.053753378638252798e+02,
-c5 = 2.367852246387234239e+00,
-c6 = 3.274658677400876881e-03,
-y00 = 1.000001,
-h0 = 0.000001,
-t0 = 1,
-t_end = -2.5;
-int booldone = 0;
+#include <stdlib.h>
+#include<stdio.h>
 
-double f(double x, double y) {
-    return -c5 * y / pow(c6 * (y - 1) + 1, 1.0 / 3.0) / pow(10, x);
-}
-double df(double x, double y) {
-    return -c5 * (3.0 + c6 * (-3 + 2 * y)) / pow(10, x) / 3 / pow(1 + c6 * (y - 1), 4.0 / 3.0);
-}
-double newton(double (*f)(double, double), double (*df)(double, double), double t, double y, double h, double y_prev, double tol) {
-    double y_next = y;
-    double y_next_old;
-    int max_iter = 100;
-    int iter = 0;
-    do {
-        y_next_old = y_next;
-        double g = y_next - (4.0 / 3.0) * y + (1.0 / 3.0) * y_prev + (2.0 / 3.0) * h * f(t, y_next);
-        double g_prime = 1 + (2.0 / 3.0) * h * df(t, y_next);
-        y_next = y_next_old - g / g_prime;
-        iter++;
-        if (iter > (max_iter - 3)) {
-            printf("error newton of t=%.17e,y=%.17e,h=%.17e", t, y, h);
-            return  NAN;
-        }
-    } while (fabs(y_next - y_next_old) > tol);
-    //printf("error of newton is %.17e,tol is%.17e\n", y_next - y_next_old,tol);
-    return y_next;
-}
-void BDF2(double (*f)(double, double), double (*df)(double, double)) {
-    double h = h0;
-    double t = t0 - h;
-    double y = y00 - h * f(t, y00);
-    double y_prev = y;
-    double min_h = 1e-15;
-    while (t > t_end) {
-        glVertex2f((GLfloat)t, log10(pow(((GLfloat)y - 1) / c2 + 1,0.33333333333333333)-1));
-        double tol = fmax(1e-7 * fabs(y - 1), 1e-9);
-        double y_half = newton(f, df, t - 0.5 * h, y, 0.5 * h, y_prev, tol);
-        double y_full = newton(f, df, t - h, y, h, y_prev, tol);
-        double y_half_full = newton(f, df, t - h, y_half, 0.5 * h, y_prev, tol);
-        double error = fabs(y_half_full - y_full) * 3.0 / 2.0;
-        //printf("h = %.17e, t = %.17e, y = %.17e, error= %.17e, y_half_full= %.17e, y_full= %.17e\n", h, t, y,error,y_half_full,y_full);
-        while (error > fmax(1e-5 * fabs(y - 1), 1e-6)) {
-            if (h < min_h) {
-                printf("Step size too small, stopping iteration.  error is %.17e,at t=%.17e\n", error, t);
-                booldone = 1;
-                return;
-            }
-            h *= 0.5;
-            y_half = newton(f, df, t - 0.5 * h, y, 0.5 * h, y_prev, tol);
-            y_full = newton(f, df, t - h, y, h, y_prev, tol);
-            y_half_full = newton(f, df, t - h, y_half, 0.5 * h, y_prev, tol);
-            printf("Reducing step size: h = %.17e,error is %.17e,y_full is %.17e y_half_full is %.17e\n", h, error, y_full, y_half_full);
-            error = fabs(y_half_full - y_full) * 3.0 / 2.0;
-        }
-        while (error < 1e-7 * fabs(y - 1)) {
-            h *= 2.2;
-            printf("Increasing step size: h = %.17e。  error is %.17e\n", h, error);
-            y_half = newton(f, df, t - 0.5 * h, y, 0.5 * h, y_prev, tol);
-            y_full = newton(f, df, t - h, y, h, y_prev, tol);
-            y_half_full = newton(f, df, t - h, y_half, 0.5 * h, y_prev, tol);
-            error = fabs(y_half_full - y_full) * 3.0 / 2.0;
-        }
-        y_prev = y;
-        y = y_full;
-        t -= h;
+float n = 0.1f;
+float a = 3.0f;
+double dt = 0.01;
+double yrem[2048] = { 0 };
+
+inline double func(double t, double y, double v) {
+    if (t == 0 && v == 0) {
+        return -pow(y, n) / 3;
     }
-    booldone = 1;
+    else { return -pow(y, n) - 2 / t * v; }
 }
 
-void init() {
-    glClearColor(1.0, 1.0, 1.0, 1.0);
-    glMatrixMode(GL_PROJECTION);
-    glLoadIdentity();
-    gluOrtho2D(-2.75, 1, -4.5, 2);
+void rk4(double t, double* y, double* v) {
+    double k1, k2, k3, k4, l1, l2, l3, l4;
+    k1 = dt * (*v);
+    l1 = dt * func(t, *y, *v);
+    k2 = dt * (*v + 0.5 * l1);
+    l2 = dt * func(t + dt / 2, *y + 0.5 * k1, *v + 0.5 * l1);
+    k3 = dt * (*v + 0.5 * l2);
+    l3 = dt * func(t + dt / 2, *y + 0.5 * k2, *v + 0.5 * l2);
+    k4 = dt * (*v + l3);
+    l4 = dt * func(t + dt, *y + k3, *v + l3);
+    *y = *y + (k1 + 2 * k2 + 2 * k3 + k4) / 6;
+    *v = *v + (l1 + 2 * l2 + 2 * l3 + l4) / 6;
 }
-void drawString(float x, float y, const char* string, void* font) {
-    glRasterPos2f(x, y);
-    while (*string) {
-        glutBitmapCharacter(font, *string++);
+
+void drawAxes() {
+    glColor3f(1.0, 1.0, 1.0);
+    glBegin(GL_LINES);
+    glVertex2f(0, 0);
+    glVertex2f(a, 0);
+    glVertex2f(0, 0);
+    glVertex2f(0, 1);
+    glEnd();
+}
+
+void drawTicks() {
+    glColor3f(1.0, 1.0, 1.0); 
+    for (float x = 0.0f; x <= a; x += 0.5f) {
+        glBegin(GL_LINES);
+        glVertex2f(x, 0);
+        glVertex2f(x, 0.02);
+        glEnd();
+        glRasterPos2f(x, -0.05);
+        char buf[10];
+        sprintf_s(buf, sizeof(buf), "%.1f", x);
+        for (char* p = buf; *p; p++) {
+            glutBitmapCharacter(GLUT_BITMAP_HELVETICA_12, *p);
+        }
+    }
+    for (float y = 0.0f; y <= 1.0f; y += 0.1f) {
+        glBegin(GL_LINES);
+        glVertex2f(0, y);
+        glVertex2f(0.03, y);
+        glEnd();
+        glRasterPos2f(-0.1, y);
+        char buf[10];
+        sprintf_s(buf, sizeof(buf), "%.1f", y);
+        for (char* p = buf; *p; p++) {
+            glutBitmapCharacter(GLUT_BITMAP_HELVETICA_12, *p);
+        }
     }
 }
+
 void display() {
-    if (!booldone) {
-        glClear(GL_COLOR_BUFFER_BIT);
-        glColor3f(0.0, 0.0, 0.0);
-        glBegin(GL_LINES);
-        glVertex2f(-3, -4);
-        glVertex2f(1, -4);
-        glEnd();
-        glBegin(GL_LINES);
-        glVertex2f(-2.5, 2);
-        glVertex2f(-2.5, -4.5);
-        glEnd();
-        for (int j = -3; j < 10; j++) {
-            glBegin(GL_LINES);
-            glVertex2f(j, -4);
-            glVertex2f(j, -3.5);
-            glEnd();
-            char buffer[3], ui[3] = { "10" };
-            snprintf(buffer, sizeof(buffer), "%d", j);
-            drawString(j + 0.04, -4.2, buffer, GLUT_BITMAP_HELVETICA_12);
-            drawString(j - 0.08, -4.3, ui, GLUT_BITMAP_HELVETICA_18);
-            for (int i = 2; i < 10; i++) {
-                glBegin(GL_LINES);
-                glVertex2f(j + log10(i), -4);
-                glVertex2f(j + log10(i), -3.8);
-                glEnd();
-            }
-        }
-        for (int j = -5; j < 2; j++) {
-            glBegin(GL_LINES);
-            glVertex2f(-2.5, j);
-            glVertex2f(-2.4, j);
-            glEnd();
-            char buffer[3], ui[3] = { "10" };
-            snprintf(buffer, sizeof(buffer), "%d", j);
-            drawString(-2.57, j, buffer, GLUT_BITMAP_HELVETICA_12);
-            drawString(-2.72, j - 0.15, ui, GLUT_BITMAP_HELVETICA_18);
-            for (int i = 2; i < 10; i++) {
-                glBegin(GL_LINES);
-                glVertex2f(-2.5, j + log10(i));
-                glVertex2f(-2.45, j + log10(i));
-                glEnd();
-            }
-        }
-        glColor3f(0.0, 0.0, 0.0);
-        glBegin(GL_LINE_STRIP);
-        BDF2(f, df);
-        glEnd();
-        glFlush();
+    glClear(GL_COLOR_BUFFER_BIT);
+    glLoadIdentity();
+    gluOrtho2D(-0.2, a, -0.07, 1);
+    drawAxes();
+    drawTicks();
+    glBegin(GL_LINE_STRIP);
+    int i = 0;
+    for (float x = 0.0f; x < a; x += 0.01f) {
+        glVertex2f(x, pow(yrem[i], n));
+        i++;
     }
+    glEnd();
+    glutSwapBuffers();
+}
+
+float caclulate(void) {
+    double y = 1.0, v = 0, t = 0;
+    int i = 0;
+    while (y > 0) {
+        yrem[i] = y;
+        rk4(t, &y, &v);
+        t += dt;
+        i++;
+    }
+    return i * dt;
+}
+
+void timer(int extra) {
+    n += 0.01f;
+    a = caclulate();
+    if (a > 20) {
+        n = 0;
+        for (int i = 0; i < 2048; i++) {
+            yrem[i] = 0;
+        }
+
+    }
+    glutPostRedisplay();
+    glutTimerFunc(30, timer, 0);
 }
 
 int main(int argc, char** argv) {
     glutInit(&argc, argv);
-    glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
-    glutInitWindowSize(500, 500);
-    glutInitWindowPosition(100, 100);
+    glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB);
+    glutInitWindowSize(700, 700); 
     glutCreateWindow("made by dhy");
-    init();
     glutDisplayFunc(display);
+    glutTimerFunc(0, timer, 0);
     glutMainLoop();
     return 0;
 }
